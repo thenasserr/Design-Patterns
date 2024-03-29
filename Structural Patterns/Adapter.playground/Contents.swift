7afa702f363fe1734ab09198b3e2f3a414219e34
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-30 01:17:58 +0300

    adapter pattern

--- Structural Patterns/Adapter.playground/Contents.swift
+++ Structural Patterns/Adapter.playground/Contents.swift
@@ -1,3 +1,43 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol MessageSender {
+    func send(message: String)
+}
+
+class SlackMessenger: MessageSender {
+    func send(message: String) {
+        print("Sent message \(message) to Slack")
+    }
+}
+
+class JabberMessenger: MessageSender {
+    func send(message: String) {
+        print("Sent message \(message) to Jabber")
+    }
+}
+
+class HipChatMessenger {
+    func send(message: String, identifier: String) {
+        print("Sent message \(message) with \(identifier) to HipChat ")
+    }
+}
+
+class HipChatMessengerAdapter: MessageSender {
+    private let hipChat = HipChatMessenger()
+    func send(message: String) {
+        hipChat.send(message: message, identifier: UUID().uuidString)
+    }
+}
+
+class Messenger {
+    private let messengers: [MessageSender] = [SlackMessenger(), JabberMessenger(), HipChatMessengerAdapter()]
+    
+    func sendToAllMessengers(message: String) {
+        for messenger in messengers {
+            messenger.send(message: message)
+        }
+    }
+}
+
+let messenger = Messenger()
+messenger.sendToAllMessengers(message: "Hello there form ibrahim")
