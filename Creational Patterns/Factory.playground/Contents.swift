871891a54a0664b81e0578312b922a23bd476182
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-29 22:09:18 +0300

    factory pattern

---
+++ Creational Patterns/Factory.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+protocol Shape {
+    func draw() -> String
+}
+
+class Rectangle: Shape {
+    func draw() -> String {
+        return "Drawing a Rectangle"
+    }
+}
+
+class Circle: Shape {
+    func draw() -> String {
+        return "Drawing a Circle"
+    }
+}
+
+class ShapeFactory {
+    func create(for shapeType: String) -> Shape? {
+        switch shapeType.lowercased() {
+            case "rectangle":
+                return Rectangle()
+            case "circle":
+                return Circle()
+            default:
+                return nil
+        }
+    }
+}
+
+let factory = ShapeFactory()
+
+var shape = factory.create(for: "rectangle")
+shape?.draw()
+shape = factory.create(for: "circle")
+shape?.draw()
+
+shape = factory.create(for: "diamond")
+shape?.draw()
